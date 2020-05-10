40a3450cf42a7f62963e5d991f6144d12685fbf3
eva <evamaria.locusteanu@gmail.com>
2020-05-10 20:49:03 +0200

    api lab 6: add put, delete operations

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(PlaceRecords, '/placerecords/', methods=['POST'])
-api.add_resource(PlaceRecord, '/placerecords/<string:name>')
+api.add_resource(PlaceRecord, '/placerecords/<string:name>',methods=['GET', 'PUT', 'DELETE'])
 #define path to the resource
 
 app.run(host='0.0.0.0', port=5000, debug=True)
