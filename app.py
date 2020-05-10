171a2be0fc0239f4d986597443ca839a8f8ee30e
eva <evamaria.locusteanu@gmail.com>
2020-05-10 19:46:33 +0200

    api lab 6: add PlaceRecords resource collection (import)

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
-from resources.place_record import PlaceRecord
+from resources.place_record import PlaceRecord, PlaceRecords
 
 app = Flask(__name__)
 api = Api(app)
