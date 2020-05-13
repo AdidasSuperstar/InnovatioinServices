83cadf362a2207de963de6551b21151e830c3ef8
eva <evamaria.locusteanu@gmail.com>
2020-05-13 21:50:19 +0200

    Fixed error 4

--- ProjectUber/EasyTripRequest/app.py
+++ ProjectUber/EasyTripRequest/app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
-from resources.trip_request import TripRequest
-
+from ProjectUber.EasyTripRequest.resources.trip_request import TripRequest
 
 app = Flask(__name__)
 api = Api(app)
