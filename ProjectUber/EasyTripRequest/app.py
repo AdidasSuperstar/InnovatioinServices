1ef45ecdc51c78b58e3544a7e1d1a896ff4bf432
eva <evamaria.locusteanu@gmail.com>
2020-05-13 21:32:15 +0200

    Fixed error

--- ProjectUber/EasyTripRequest/app.py
+++ ProjectUber/EasyTripRequest/app.py
@@ -1,15 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 
-from resources.trip_request import TripRequest, TripRequests, TripsRequests
+from resources.trip_request import TripRequest
 from resources.drivers import DriverRecord, DriverRecords, DriversRecords
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(TripRequests, '/trip_requests/', methods=['POST'])
-api.add_resource(TripRequest, '/trip_requests/<string:name>', methods=['GET', 'PUT', 'DELETE'])
-api.add_resource(TripsRequests, '/trip_requests/', methods=['GET'])
+api.add_resource(TripRequest, '/trip_requests/<string:destination>', methods=['GET'])
 
 app.run(host='0.0.0.0', port=5000, debug=True)
 # In the context of servers, 0.0.0.0 can mean "all IPv4 addresses on the local machine".
